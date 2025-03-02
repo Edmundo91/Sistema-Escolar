3a9b2b26f66430dde8da9c7718db4bc962341474
Edmundo Duarte <edmundoduarte1998@gmail.com>
2025-03-01 21:39:07 -0300

    implementando a classe Usercontroller

--- src/main/java/com/sistemaescolar/controllers/UserController.java
+++ src/main/java/com/sistemaescolar/controllers/UserController.java
@@ -1,5 +1,52 @@
 package com.sistemaescolar.controllers;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sistemaescolar.models.User;
+import com.sistemaescolar.services.UserService;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+ private UserService service;
+	
+
+ @PostMapping("/save")
+ public User registrarUser(User user) { 
+	 
+	return service.SalvarUser(user); 
+	 
+ }
+ 
+ 
+ @GetMapping("/{id}")
+ public User buscarUserPorId(Long id) { 
+	 
+	 return service.buscarUserPorId(id);
+	 
+ }
+	
+ 
+ 
+@GetMapping("/busca")    
+public List<User> buscarUserPorNome(String nome){ 
+		
+	return service.buscarUserPorNome(nome);
+				
+	}
+ 
+ @DeleteMapping("/{id}")
+ public void deletarUser(Long id) { 
+	 
+     service.DeleteUser(id);	 
+	 
+ }
+ 
+	
 }
